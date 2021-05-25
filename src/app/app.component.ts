d478d04b8adb12f347105004113d45602e895a11
shev4ukyuri@gmail.com <yshevc@softserveinc.com>
2021-05-25 20:41:20 +0300

    lesson 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'course-angular';
+  title = 'Hello world';
+
+  user = {
+    name: 'Bob',
+    age: 28
+  };
+
+  products = [{
+    name: 'Product 1',
+    price: 300
+  }, {
+    name: 'Product 2',
+    price: 30
+  }, {
+    name: 'Product 5',
+    price: 50
+  }];
+
+  showDropDown = false;
+
+  constructor () {
+    setTimeout(() => {
+      this.title = 'New title';
+      // debugger
+    }, 3000);
+
+    console.log(this.title);
+  }
+
+  toggleDropDown() {
+    // alert('click');
+    this.showDropDown = !this.showDropDown;
+  }
 }
