6815f2c632a0d3439ed9162c16a57c45ddaa795b
shev4uk <shev4ukyuri@gmail.com>
2021-06-17 19:27:07 +0300

    add custom validation

--- src/app/reactive-form/reactive-form.component.ts
+++ src/app/reactive-form/reactive-form.component.ts
@@ -1,6 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
+function confirmPassword(passwordField: string, confirmPasswordField: string) {
+  return (formGroup: FormGroup) => {
+    const password = formGroup.controls[passwordField];
+    const confirmPassword = formGroup.controls[confirmPasswordField];
+    console.log(password, confirmPassword);
+
+    if (password.value !== confirmPassword.value) {
+      confirmPassword.setErrors({ confirm: true });
+    } else {
+      confirmPassword.setErrors(null);
+    }
+  }
+}
+
 @Component({
   selector: 'app-reactive-form',
   templateUrl: './reactive-form.component.html',
@@ -21,8 +35,9 @@ export class ReactiveFormComponent implements OnInit {
   ngOnInit(): void {
     this.loginForm = this.fb.group({
       loginName: ['login', [Validators.required]],
-      passwordForm: ['123', [Validators.required, Validators.minLength(8)]]
-    })
+      passwordForm: ['123', [Validators.required, Validators.minLength(8)]],
+      confirmPassword: ['', [Validators.required, Validators.minLength(8)]]
+    }, {validator: confirmPassword('passwordForm', 'confirmPassword')})
   }
 
   loginUser() {
